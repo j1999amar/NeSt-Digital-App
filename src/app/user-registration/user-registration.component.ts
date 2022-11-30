3ad8c893e82827149e3dad113235f9fc2b4f6931
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-30 19:25:44 +0530

    Read User Registration

--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-registration',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-registration.component.css']
 })
 export class UserRegistrationComponent {
+  constructor( private route:Router ){}
+  empname=""
+  empId=""
+  firstName=""
+  lastName=""
+  houseNo=""
+  houseName=""
+  streetName=""
+  pincode=""
+  district=""
+  state=""
+  country=""
+  mobile=""
+  email=""
+  parrentName=""
+  gender=""
+  degree=""
+  exp=""
+  joining=""
+  username=""
+  password=""
+  confrimPassword=""
+
+  readValue=()=>{
+    if(this.password==this.confrimPassword){
+      this.route.navigate(['/userLogin'])
+    }else{
+      alert("Login Failed")
+      this.empname=""
+      this.empId=""
+      this.firstName=""
+      this.lastName=""
+      this.houseNo=""
+      this.houseName=""
+      this.streetName=""
+      this.pincode=""
+      this.district=""
+      this.state=""
+      this.country=""
+      this.mobile=""
+      this.email=""
+      this.parrentName=""
+      this.gender=""
+      this.degree=""
+      this.exp=""
+      this.joining=""
+      this.username=""
+      this.password=""
+      this.confrimPassword=""
+    }
+  }
+
+
+
+
+
 
 }
